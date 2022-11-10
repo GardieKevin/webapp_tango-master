62e1d565f667f119bc14827fee810582644ac6fe
GardieKevin <gardie.kevin@gmail.com>
2022-11-10 02:12:42 +0100

    Attribute "required" in forms Differenciation H/F for dancers in lesson page Conditionnal display of the datas and attendees in lesson page

--- webapp_tango-master/src/main/java/com/gardie/webapptango/controller/LessonController.java
+++ webapp_tango-master/src/main/java/com/gardie/webapptango/controller/LessonController.java
@@ -1,5 +1,6 @@
 package com.gardie.webapptango.controller;
 
+import com.gardie.webapptango.model.Dancer;
 import com.gardie.webapptango.model.Lesson;
 import com.gardie.webapptango.service.DancerService;
 import com.gardie.webapptango.service.LessonService;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Controller
@@ -51,8 +55,30 @@ public class LessonController {
 
 	@GetMapping("/getLesson/{id}")
 	public String createLDAssociation(@PathVariable("id") final int id, Model model) {
+
 		Lesson l = lessonService.getLesson(id);
+		List<Dancer> attendees = l.getAttendees();
+		List<Dancer> males = new ArrayList<>();
+		List<Dancer> females = new ArrayList<>();
+		int maleCount = 0;
+		int femaleCount = 0;
+
+		for(Dancer attendee : attendees){
+			if (attendee.getGender().equals("Homme")){
+				males.add(attendee);
+				maleCount ++;
+			} else {
+				females.add(attendee);
+				femaleCount ++;
+			}
+		}
+
+		model.addAttribute("males", males);
+		model.addAttribute("females", females);
 		model.addAttribute("lesson", l);
+		model.addAttribute("maleCount", maleCount);
+		model.addAttribute("femaleCount", femaleCount);
+
 		return "aLesson";
 	}
 	
